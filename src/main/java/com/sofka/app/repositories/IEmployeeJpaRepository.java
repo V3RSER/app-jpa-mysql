e64c4db73a6a0e86416f9f3b88ac375fc9992936
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-20 18:43:12 -0500

    Agregado repositorio de empleado

---
+++ src/main/java/com/sofka/app/repositories/IEmployeeJpaRepository.java
@@ -0,0 +1,12 @@
+package com.sofka.app.repositories;
+
+import com.sofka.app.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Se establece que es un repositorio
+public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
+    // Devuelve un empleado que coincida con una id pasada como parámetro
+    Employee findByEmployeeid(String employeeid);
+
+}
